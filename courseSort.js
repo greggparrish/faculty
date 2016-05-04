cc3f6fb64d537946536987e68511f0b604884840
Gregg Parrish <gregg.alb@gmail.com>
2016-05-04 22:09:56 +0100

    Add spaces around course number

--- courseSort.js
+++ courseSort.js
@@ -142,7 +142,7 @@ buildArray = function(sortOrder) {
   courseCount = function(itemCount) {
     if(itemCount !== undefined) {
       if(itemCount < 200) {
-        $('.showing-courses').text( "Found" + itemCount + "courses"); 
+        $('.showing-courses').text( "Found " + itemCount + " courses"); 
       }
       else if (itemCount > 200) {
         $('.showing-courses').text('Found more than 200'); 
